9dad71bd697f4acea235ef9b773a4af9195d7c69
thePerfesser <perfesser100@gmail.com>
2017-11-14 21:06:06 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # csc-470
 Repo for CSC 470 Group Project. Fall 17 @ DSU
+
+Project "cookN"
---
+++ RecipeIngredient.cpp
@@ -0,0 +1,25 @@
+// RecipeIngredient.cpp
+#include "RecipeIngredient.h"
+
+using std::string;
+
+RecipeIngredient::RecipeIngredient( string measurement, string name) {
+  this->measurement = measurement;
+  this->name = name;
+}
+
+string RecipeIngredient::getMeasurement() {
+  return measurement;
+}
+
+string RecipeIngredient::getName() {
+  return name;
+}
+
+void RecipeIngredient::setMeasurement(string measurement) {
+  this->measurement = measurement;
+}
+
+void RecipeIngredient::setName(string name) {
+  this->name = name;
+}
