11ce7a04f6d30db5903038460eac6dd0a036e9e3
AlFnC <32488888+AlFnC@users.noreply.github.com>
2017-11-16 02:33:02 -0500

    Create ingredient.h

---
+++ @early-code-and-tests/ingredient - date/ingredient.h
@@ -0,0 +1,30 @@
+// Ingredient.h
+
+#ifndef INGREDIENT_H
+#define INGREDIENT_H
+
+#include <string>
+#include <vector>
+#include "Date.h"
+
+
+class Ingredient {
+
+ private:
+  std::string name;
+  int quantity;
+  std::vector<Date> expirationDates;
+
+  void setName(std::string name);
+  void setQuantity(int quantity);
+  void addExpirationDate(std::vector<Date> expirationDates);
+
+ public:
+  Ingredient( std::string name, int quantity, std::vector<Date> expirationDates);
+  std::string getName();
+  int getQuantity();
+  std::vector<Date> getExpirationDates();
+  
+};
+
+#endif
