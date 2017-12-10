2fab42e439105cae698286a136338444f9a5305d
ThePerfesser <perfesser100@gmail.com>
2017-12-09 23:04:22 -0700

    scrub expdate Ingredient.h

--- KitchN-console/Ingredient.h
+++ KitchN-console/Ingredient.h
@@ -4,29 +4,21 @@
 #define INGREDIENT_H
 
 #include <string>
-#include <vector>
-#include "Date.h"
-
 
 class Ingredient {
 
  private:
   std::string name;
   int quantity;
-  std::vector<Date> expirationDates;
 
  public:
+  Ingredient( std::string name, int quantity);
+
   void setName(std::string name);
   void setQuantity(int quantity);
-  //void addExpirationDate(std::vector<Date> expirationDates);
-  //void addExpirationDate(Date date);
-  void addExpirationDate(int month, int day, int year);
 
-  Ingredient( std::string name, int quantity, std::vector<Date> expirationDates);
   std::string getName();
-  int getQuantity();
-  std::vector<Date> getExpirationDates();
-  
+  int getQuantity();  
 };
 
 #endif
